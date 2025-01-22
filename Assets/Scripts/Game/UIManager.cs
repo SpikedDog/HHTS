09960372611e989722428c842046bef4d18d16b4
KAY <123421289+SpikedDog@users.noreply.github.com>
2025-01-22 17:30:22 +0000

    Test V5.5
    Wiped test and working on customer default

--- Assets/Scripts/Game/UIManager.cs
+++ Assets/Scripts/Game/UIManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+    private static UIManager instance;
     public Text timerText;
     public Text pointsText;
     public Text objectivesText;
@@ -15,6 +16,14 @@ public class UIManager : MonoBehaviour
 
     void Start()
     {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         UpdateUI();
     }
 
